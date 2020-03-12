f83dfa674c4d39111dda68d847dfb1c823fadb2a
nazimkazim <nazik_111@mail.ru>
2020-03-12 16:24:37 +0600

    add style header

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ el.addEventListener("click", function() {
 console.log(lesson);
 
 const lessonPlateNumber = document.querySelector(".lesson-plate-number");
-lessonPlateNumber.innerText += `${lesson.header.number}`;
+lessonPlateNumber.innerText = `${lesson.header.number}`;
 
 const lessonPlateTitle = document.querySelector(".lesson-plate-title");
-lessonPlateTitle.innerText += `${lesson.header.title}`;
+lessonPlateTitle.innerText = `${lesson.header.title}`;
+
+const lessonPlateDesc = document.querySelector(".lesson-plate-desc");
+lessonPlateDesc.innerText = `${lesson.header.grammar}`;
+
+const lessonPlateCandoDesc = document.querySelector(".lesson-plate-cando-desc");
+lessonPlateCandoDesc.innerText = `${lesson.header.can_do}`;
